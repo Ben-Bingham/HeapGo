6ed58ad5e203f07dd85720cbc624a039e13256da
Ben Bingham <ben@paynes-gray.lan>
2026-09-02 08:52:02 -0600

    Implemented basic command loop

---
+++ heapGo.py
@@ -0,0 +1,8 @@
+G = [ [('w', 9), ('b', 12), ('w', 5)],  [('b', 6), ('b', 9)] ]
+
+print("Welcome to HeapGo")
+
+while True:
+    print("Enter your command:")
+    command = input()
+    print(command)
